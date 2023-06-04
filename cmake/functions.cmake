f6fa487e5e52eea86dc0bbb68e7dafcf64f3088b
Silvio Traversaro <silvio@traversaro.it>
2023-06-04 16:46:12 +0200

    Disable werror

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -82,12 +82,12 @@ macro(google_initialize_cartographer_project)
     google_add_flag(GOOG_CXX_FLAGS "-Wpedantic")
 
     # Turn some warnings into errors.
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=format-security")
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=missing-braces")
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=reorder")
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=return-type")
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=switch")
-    google_add_flag(GOOG_CXX_FLAGS "-Werror=uninitialized")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=format-security")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=missing-braces")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=reorder")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=return-type")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=switch")
+    #google_add_flag(GOOG_CXX_FLAGS "-Werror=uninitialized")
 
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "AppleClang")
       google_add_flag(GOOG_CXX_FLAGS "-Wthread-safety")
